86e709fc120b05461bcb3e5bec19f00135c4f038
rajrajeshwarsingh03 <rajrajeshwarsingh03@gmail.com>
2024-08-10 18:34:40 +0530

    Components added & react hook s introduced

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,24 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, avgRatingString, sla, cuisines, areaName } =
+    resData?.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3>{name}</h3>
+      <h4>
+        ⭐{avgRatingString} . {sla?.slaString}
+      </h4>
+      <h4 className="truncate">{cuisines.join(", ")}</h4>
+      <h4>{areaName}</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
